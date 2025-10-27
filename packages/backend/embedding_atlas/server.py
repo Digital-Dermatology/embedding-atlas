0bc416320745a596e7deac47bb52eaa636b29501
Fabian Gröger <fabian.groeger@bluewin.ch>
2025-10-27 19:32:34 +0100

    DB issues

--- packages/backend/embedding_atlas/server.py
+++ packages/backend/embedding_atlas/server.py
@@ -56,8 +56,8 @@ def make_server(
             db_meta = {"database": {"type": "wasm", "load": True}}
         elif duckdb_uri == "server":
             # Point to the server itself.
-            # Note: load=False because the server already has the data in memory
-            db_meta = {"database": {"type": "rest"}}
+            # Note: We need to set URI explicitly for the REST connector
+            db_meta = {"database": {"type": "rest", "uri": "/data/query"}}
         else:
             # Point to the given uri.
             if duckdb_uri.startswith("http"):
